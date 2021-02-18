6c0879bbd7b855ad6ffc0f8afbe8f9c513ea706d
Dancan Chibole <dancankingstar@gmail.com>
2021-02-17 18:08:08 -0800

    requests

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MdSearch } from 'react-icons/md';
-import { GithubContext } from '../context/context';
+import React from "react";
+import styled from "styled-components";
+import { MdSearch } from "react-icons/md";
+import { GithubContext } from "../context/context";
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = React.useState("");
+  const { request } = React.useContext(GithubContext);
+  const handleSubmit = () => {};
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input
+              type="text"
+              placeholder="enter github user"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+          </div>
+        </form>
+      </Wrapper>
+      <h3>requests : {request} / 60</h3>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
--- src/context/context.js
+++ src/context/context.js
@@ -12,8 +12,30 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  //requests and loading
+  const [request, setRequest] = useState(0);
+  const [loading, setIsLoading] = useState(false);
+
+  const checkRequestsLimit = async () => {
+    try {
+      const { data } = await axios.get(`${rootUrl}/rate_limit`);
+      let {
+        rate: { remaining },
+      } = data;
+      setRequest(remaining);
+      if (remaining === 0) {
+        //throw an error
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  //errors
+  useEffect(() => {
+    checkRequestsLimit();
+  }, []);
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider value={{ request, githubUser, repos, followers }}>
       {children}
     </GithubContext.Provider>
   );
